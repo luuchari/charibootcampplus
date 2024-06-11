2dd60038fc7807b2db3866d2ad7efeca844fbf6e
luuchari <mthombeninhlaluko2@gmail.com>
2024-06-11 04:45:21 +0200

    second commit

---
+++ variables.js
@@ -0,0 +1,48 @@
+// Exercise 1: Variable Declaration
+// 1. Declare a variable ‘name’ and assign your name to it.
+var name = "Mthombeni charity Nhlaluko";
+
+// 2. Declare a variable ‘age’ and assign your age to it.
+var age = 25;
+
+// 3. Declare a variable ‘isStudent’ and assign it to a boolean value
+// indicating whether you are a student or not.
+var isStudent = true;
+
+// 4. Print all three variables.
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Is Student:", isStudent);
+
+// Exercise 2: Variable Reassignment
+// 1. Declare a variable ‘favoriteColour’ and assign your favourite
+// colour to it.
+var favoriteColor = "blue";
+
+// 2. Print the value of favoriteColor.
+console.log("Favorite Color:", favoriteColor);
+
+// 3. Reassign favoriteColor to a different color.
+favoriteColor = "Brown";
+
+// 4. Print the new value of favoriteColor.
+console.log("New Favorite Color:", favoriteColor);
+
+// Exercise 3: Arithmetic Operations
+// 1. Declare two variables ‘num1’ and ‘num2’ and assign them
+// any two numbers.
+var num1 = 10;
+var num2 = 5;
+
+// 2. Declare variables to store the sum, difference, product, and
+// quotient of num1 and num2.
+var sum = num1 + num2;
+var difference = num1 - num2;
+var product = num1 * num2;
+var quotient = num1 / num2;
+
+// 3. Print the results of these arithmetic operations.
+console.log("Sum:", sum);
+console.log("Difference:", difference);
+console.log("Product:", product);
+console.log("Quotient:", quotient);
